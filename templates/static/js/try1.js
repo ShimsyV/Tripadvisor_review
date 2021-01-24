9ebe06ee4dedb5e61b887b54f91e0a8f3ea888da
Justin <duckweiser@gmail.com>
2021-01-24 09:23:36 -0600

    visualizations work

---
+++ templates/static/js/try1.js
@@ -0,0 +1,3 @@
+//json lookup
+var jsontarget = "https://media.githubusercontent.com/media/jebreensa/Project-3_Group1/main/output/Reviews_Cutoff_Clusters_Keywords.csv"
+// var jsontarget = "/api/v1.0/alldrugs"
