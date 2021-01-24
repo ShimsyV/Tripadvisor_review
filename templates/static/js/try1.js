bc1f8a0f45b953b07aa606990f37a20eb6f860bd
Justin <duckweiser@gmail.com>
2021-01-24 13:57:26 -0600

    added python/html based graphs, still figuring out how to work on it in js

--- templates/static/js/try1.js
+++ templates/static/js/try1.js
@@ -1,3 +1,30 @@
 //json lookup
-var jsontarget = "https://media.githubusercontent.com/media/jebreensa/Project-3_Group1/main/output/Reviews_Cutoff_Clusters_Keywords.csv"
+var jsontarget = "https://media.githubusercontent.com/media/jebreensa/Project-3_Group1/main/output/sunburst_chart_ref.csv"
 // var jsontarget = "/api/v1.0/alldrugs"
+
+// CHANGE THIS TO JSON WHEN SOURCE DATA IS READY
+Plotly.d3.csv(jsontarget, function(err, rows){
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+}
+
+var data1 = [{
+	"type": "sunburst",
+	"maxdepth": 2,
+	"labels": unpack(rows,"reviews_rating"),
+	"parents": unpack(rows,"cluster"),
+	"values": unpack(rows,"Count"),
+	}];
+		
+var layout = {
+  margin: {l: 0, r: 0, b: 0, t: 0},
+  width: 500,
+  height: 500
+};	
+		
+
+Plotly.newPlot('plot1', data1, layout)
+});
+
+	
+//province,city,reviews_rating,cluster,keywords,Count
