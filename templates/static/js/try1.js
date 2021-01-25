97796e20342bff9677f03cfb8b34823be73c84ad
Justin <duckweiser@gmail.com>
2021-01-25 05:55:19 -0600

    more work on frontend

--- templates/static/js/try1.js
+++ templates/static/js/try1.js
@@ -1,30 +1,113 @@
 //json lookup
 var jsontarget = "https://media.githubusercontent.com/media/jebreensa/Project-3_Group1/main/output/sunburst_chart_ref.csv"
-// var jsontarget = "/api/v1.0/alldrugs"
+// var jsontarget = "/api/v1.0/reviewsall"
 
 // CHANGE THIS TO JSON WHEN SOURCE DATA IS READY
 Plotly.d3.csv(jsontarget, function(err, rows){
-  function unpack(rows, key) {
-  return rows.map(function(row) { return row[key]; });
+	function unpack(rows, key) {
+	return rows.map(function(row) { return row[key]; });
 }
 
+
 var data1 = [{
-	"type": "sunburst",
-	"maxdepth": 2,
-	"labels": unpack(rows,"reviews_rating"),
-	"parents": unpack(rows,"cluster"),
-	"values": unpack(rows,"Count"),
+	type: 'bar',
+	y: unpack(rows, 'city'),
+	x: unpack(rows, 'Count'),
+	marker: {color: 'rgb(255, 83, 109)'},
+	orientation: 'h',
+	transforms: [
+	{
+		type: 'aggregate',
+		groups: unpack(rows, 'Count'),
+		aggregations: [
+		{target: 'X', func: 'sum'},
+		]
+	}
+	]
 	}];
-		
-var layout = {
-  margin: {l: 0, r: 0, b: 0, t: 0},
-  width: 500,
-  height: 500
-};	
-		
+  
+  var layout1 = {
+	  // title: "Comparison 1",
+	  autosize: false,
+	//   height: 1000,
+	  xaxis: { title: "<b>Number of Available Reviews</b>"},
+	//   yaxis: { title: "<b>Review Count</b>"}
 
-Plotly.newPlot('plot1', data1, layout)
-});
+	};
+  
+  Plotly.newPlot('plot1', data1, layout1)
+    });
 
+Plotly.d3.csv(jsontarget, function(err, rows){
+	function unpack(rows, key) {
+	return rows.map(function(row) { return row[key]; });
+}
+
+
+var data1 = [{
+	type: 'bar',
+	y: unpack(rows, 'city'),
+	x: unpack(rows, 'Count'),
+	marker: {color: 'rgb(255, 83, 109)'},
+	orientation: 'h',
+	transforms: [
+	{
+		type: 'aggregate',
+		groups: unpack(rows, 'Count'),
+		aggregations: [
+		{target: 'X', func: 'sum'},
+		]
+	}
+	]
+	}];
+  
+  var layout1 = {
+	  title: "Sample of Cities with Available Reviews Within Threshold (10+ Reviews)",
+	  autosize: false,
+	//   height: 1000,
+	  xaxis: { title: "<b>Number of Available Reviews</b>"},
+	//   yaxis: { title: "<b>Review Count</b>"}
+
+	};
+  
+  Plotly.newPlot('plot1', data1, layout1)
+    });
+
+Plotly.d3.csv(jsontarget, function(err, rows){
+	function unpack(rows, key) {
+	return rows.map(function(row) { return row[key]; });
+}
+
+
+var data2 = [{
+	type: 'bar',
+	y: unpack(rows, 'reviews_rating'),
+	x: unpack(rows, 'Count'),
+	// marker: {color: 'rgb(255, 83, 109)'},
+	orientation: 'h',
+	transforms: [
+	{
+		type: 'aggregate',
+		groups: unpack(rows, 'Count'),
+		aggregations: [
+		{target: 'X', func: 'count'},
+		]
+	}
+	]
+	}];
 	
+	var layout2 = {
+		title: "Count of Review Ratings Within Threshold (10+ Reviews)",
+		autosize: true,
+	//   height: 1000,
+		xaxis: { title: "<b>Number of Available Reviews</b>"},
+	//   yaxis: { title: "<b>Review Count</b>"}
+
+	};
+	
+	Plotly.newPlot('plot2', data2, layout2)
+	});
+
+
+
 //province,city,reviews_rating,cluster,keywords,Count
